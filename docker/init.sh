b7ee0f8406ab3f3bed4f3a946f306e7bc7481633
dbierer <doug@unlikelysource.com>
2021-03-27 12:21:38 +0700

    2021-03-27 DB: Updated infrastructure

--- docker/init.sh
+++ docker/init.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 # Usage: phpmyadmin_install.sh VERSION
 
-echo "Installing Composer ..."
-cd /tmp
-wget https://getcomposer.org/composer.phar
-mv /tmp/composer.phar /usr/local/bin/composer
-chmod +x /usr/local/bin/composer
-
 echo "Installing Guestbook ..."
 cd /home/guestbook
 composer install
@@ -17,14 +11,13 @@ composer install
 
 echo "Setting up Apache ..."
 echo "ServerName laminas" >> /etc/httpd/httpd.conf
-ln -s /home/sandbox/public /srv/www/sandbox && \
-ln -s /home/guestbook/public /srv/www/guestbook && \
-ln -s /home/onlinemarket.complete/public /srv/www/onlinemarket.complete
+ln -f -s /home/sandbox/public /srv/www/sandbox
+ln -f -s /home/guestbook/public /srv/www/guestbook
+ln -f -s /home/onlinemarket.complete/public /srv/www/onlinemarket.complete
 
 echo "Setting permissions ..."
-chown apache:apache /srv/www && \
-chgrp -R apache /home/*
-chmod -R 775 /home/*
+chmod +x /tmp/*.sh
+/tmp/reset_perms.sh
 
 echo "Initializing MySQL, PHP-FPM and Apache ... "
 /etc/init.d/mysql start
